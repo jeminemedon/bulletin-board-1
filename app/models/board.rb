e87225ddf6dbe007897af169d6ecfc37e81b2da3
Jemzy <169238907+jeminemedon@users.noreply.github.com>
2025-05-25 18:07:11 +0000

    First and last commit

---
+++ app/models/board.rb
@@ -0,0 +1,13 @@
+# == Schema Information
+#
+# Table name: boards
+#
+#  id         :bigint           not null, primary key
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+class Board < ApplicationRecord
+  has_many :posts
+end
+
